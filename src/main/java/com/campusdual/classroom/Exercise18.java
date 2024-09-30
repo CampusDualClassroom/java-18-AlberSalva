53830174154555790d82cc93d4f232c0bf0ea99c
Alberto Salvado Fernández <95777814+AlberSalva@users.noreply.github.com>
2024-09-30 23:05:36 +0200

    Implemented createAndInitializeArray(), showInlineArray and used Utils integer() for user input

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -3,6 +3,28 @@ package com.campusdual.classroom;
 public class Exercise18 {
 
 	public static void main(String[] args) {
-
+		int value = com.campusdual.util.Utils.integer();
+		int[] array = createAndInitializeArray(value);
+		showInlineArray(array);
 	}
+	public static int[] createAndInitializeArray(int num) {
+		int[] intArray = new int[num];
+		for (int i = 0; i < num; i++) {
+			intArray[i] = (i + 1);
+		}
+		return intArray;
+	}
+
+	public static void showInlineArray(int[] array) {
+		StringBuilder arrString = new StringBuilder();
+		for (int i = 0; i < array.length; i++) {
+			arrString.append(array[i]);
+			if (array.length - i > 1) {
+				arrString.append(" ");
+			}
+
+		}
+		System.out.println(arrString);
+	}
+
 }
